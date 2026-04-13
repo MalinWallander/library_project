0f35f1e17578a5e67eb7da5ea4aadcd814843f75
klarahuss <karaso-5@student.ltu.se>
2026-04-13 09:22:33 -0600

    tillägg klass kurslitteratur

--- src/main/java/com/library/model/items/CourseLiterature.java
+++ src/main/java/com/library/model/items/CourseLiterature.java
@@ -1,5 +1,14 @@
 package com.library.model.items;
 
-public class CourseLiterature {
+public class CourseLiterature extends Item {
+    private String courseName;
+    private String author;
+
+    public CourseLiterature(String itemId, String itemTitle, String categoryId, String courseName, String author) {
+        super(itemId, "CourseLiterature", itemTitle, categoryId);
+        this.courseName = courseName;
+        this.author = author;
+    }
+
 
 }
