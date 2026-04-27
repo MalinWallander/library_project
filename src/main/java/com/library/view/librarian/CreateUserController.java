79088f9bd42b249dc18ec8e0f0797086c6af4265
MalinW <maalinwallander@gmail.com>
2026-04-27 23:11:41 +0200

    fix conflict, revert project name change

--- src/main/java/com/library/view/librarian/CreateUserController.java
+++ src/main/java/com/library/view/librarian/CreateUserController.java
@@ -8,28 +8,28 @@ import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.ComboBox;
 import javafx.scene.control.DatePicker;
-<<<<<<< HEAD
-import javafx.scene.control.TextField;
-import javafx.stage.Stage;
-=======
 import javafx.scene.control.Label;
 import javafx.scene.control.PasswordField;
 import javafx.scene.control.TextField;
 import javafx.stage.Stage;
-import com.library.service.UserService;
-
-import java.time.LocalDate;
->>>>>>> Authentication
 
 public class CreateUserController {
-    @FXML private TextField fNameField;
-    @FXML private TextField lNameField;
-    @FXML private TextField emailField;
-    @FXML private TextField phoneField;
-    @FXML private ComboBox<String> roleDropdown;
-    @FXML private DatePicker dobPicker;
-    @FXML private PasswordField passwordField;
-    @FXML private Label messageLabel;
+    @FXML
+    private TextField fNameField;
+    @FXML
+    private TextField lNameField;
+    @FXML
+    private TextField emailField;
+    @FXML
+    private TextField phoneField;
+    @FXML
+    private ComboBox<String> roleDropdown;
+    @FXML
+    private DatePicker dobPicker;
+    @FXML
+    private PasswordField passwordField;
+    @FXML
+    private Label messageLabel;
 
     private UserService userService;
 
